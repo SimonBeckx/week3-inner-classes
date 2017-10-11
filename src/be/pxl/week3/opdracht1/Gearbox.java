753568b34c90fc460365d943570a8f83b61991ce
Simon <Simon@DESKTOP-4I7GFI9>
2017-10-11 10:19:03 +0200

    Opdracht 1 volledig ( testGearboxConstructorAndWheelSpeed werkt niet )

--- src/be/pxl/week3/opdracht1/Gearbox.java
+++ src/be/pxl/week3/opdracht1/Gearbox.java
@@ -1,38 +1,68 @@
 package be.pxl.week3.opdracht1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import sun.net.www.content.text.plain;
 
 public class Gearbox {
-	
+
 	private int maxGears;
 	private boolean clutchIsIn;
+	private int currentGear;
 	private List<Gear> array = new ArrayList<>();
-	
+
 	public Gearbox(int maxGears) {
 		this.maxGears = maxGears;
+		for (int i = 0; i < maxGears; i++) {
+			Gear newGear = new Gear();
+			newGear.gearNumber = i;
+			newGear.ratio = i * 5.3;
+			addGear(newGear);
+		}
 	}
-	
+
 	public void operateClutch(boolean in) {
 		clutchIsIn = in;
 	}
-	
-	public void addGear(Gear gear) {
+
+	private void addGear(Gear gear) {
 		array.add(gear);
 	}
-	
+
+	public void changeGear(int newgear) {
+		if (clutchIsIn == true) {
+			if (newgear < maxGears) {
+				System.out.println("Gear : " + newgear + " Selected.");
+				currentGear = newgear;
+			}
+		} else {
+			System.out.println("GRRAAAHHGG");
+			System.out.println("Gear : 0 Selected." );
+			currentGear = 0;
+		}
+	}
+
+	public double wheelSpeed(int revs) {
+		if (clutchIsIn == true) {
+			System.out.println("GRRAAAHGG");
+			return 0;
+		}else {
+			return array.get(currentGear).driveSpeed(revs);
+		}
+	}
+
 	public class Gear {
 		private int gearNumber;
 		private double ratio;
-		
+
 		public double driveSpeed(int revs) {
 			double param;
-			param = revs*ratio;
-			
+			param = revs * ratio;
+
 			return param;
 		}
 	}
-	
-	
 }
-	
